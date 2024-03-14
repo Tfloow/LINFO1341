702f6f6d27838b602d30ad7708ba27210447ae28
MatyaAydin <matyayin@gmail.com>
2024-03-14 16:27:56 +0100

    Function process_with_scapy and new capture during the download of a file

---
+++ P1/datanalyse.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from scapy.all import *
+
+
+
+def process_with_scapy(fileName):
+    protocol_count = {}
+
+    pcap_data = rdpcap(fileName)
+    sessions = pcap_data.sessions()
+    for session in sessions:
+        for packet in sessions[session]:
+            for i in range(len(packet.layers())):
+                layer = packet.getlayer(i)
+                protocol = layer.name
+
+                # Count the number of occurences for each protocol type
+                if protocol not in protocol_count: protocol_count[protocol] = 1
+                else: protocol_count[protocol] += 1
+
+    # Sort the dictionary in descending order
+    protocol_count = dict(sorted(protocol_count.items(), key=lambda item: item[1], reverse=True))
+    
+    # Print the output
+    for protocol in protocol_count:
+        print(f'{protocol_count[protocol]} packets have layer "{protocol}"')
+
+process_with_scapy("trace/Chrome_Eduroam_downloading_txtfile_from_Onedrive/ssl_capture.pcap")
